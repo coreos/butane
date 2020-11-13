272e84166cb53c3351765a33ff6e8ba9039ed7ba
Benjamin Gilbert <bgilbert@redhat.com>
2020-11-13 18:50:38 -0500

    base: move test helper functions to utility package

---
+++ base/util/test.go
@@ -0,0 +1,67 @@
+// Copyright 2020 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.)
+
+package util
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/coreos/fcct/translate"
+
+	"github.com/clarketm/json"
+)
+
+// helper functions for writing tests
+
+// VerifyTranslations ensures all the translations are identity, unless they
+// match a listed one, and verifies that all the listed ones exist.
+// it returns the offending translation if there is one
+func VerifyTranslations(set translate.TranslationSet, exceptions ...translate.Translation) *translate.Translation {
+	exceptionSet := translate.TranslationSet{
+		FromTag: set.FromTag,
+		ToTag:   set.ToTag,
+		Set:     map[string]translate.Translation{},
+	}
+	for _, ex := range exceptions {
+		exceptionSet.AddTranslation(ex.From, ex.To)
+		if tr, ok := set.Set[ex.To.String()]; ok {
+			if !reflect.DeepEqual(tr, ex) {
+				return &ex
+			}
+		} else {
+			return &ex
+		}
+	}
+	for key, translation := range set.Set {
+		if ex, ok := exceptionSet.Set[key]; ok {
+			if !reflect.DeepEqual(translation, ex) {
+				return &ex
+			}
+		} else if !reflect.DeepEqual(translation.From.Path, translation.To.Path) {
+			return &translation
+		}
+	}
+	return nil
+}
+
+/// FormatJSON serializes the input to formatted JSON for display when a
+/// test fails
+func FormatJSON(from interface{}) string {
+	buf, err := json.MarshalIndent(from, "", "  ")
+	if err != nil {
+		return fmt.Sprintf("<Error marshaling to JSON: %v>", err)
+	}
+	return string(buf)
+}
