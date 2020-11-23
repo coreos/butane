a8ae63de0b7645cf625911f71bf4e15227743ef4
Benjamin Gilbert <bgilbert@redhat.com>
2020-11-23 12:55:05 -0500

    base/util: add function to merge configs and TranslationSets
    We can use this to implement desugaring by generating a separate
    desugared config and merging it with the translated one.

---
+++ base/util/merge.go
@@ -0,0 +1,61 @@
+// Copyright 2020 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.)
+
+package util
+
+import (
+	"fmt"
+
+	"github.com/coreos/fcct/translate"
+
+	"github.com/coreos/ignition/v2/config/merge"
+)
+
+// MergeTranslatedConfigs merges a parent and child config and returns the
+// result.  It also generates and returns the merged TranslationSet by
+// mapping the parent/child TranslationSets through the merge transcript.
+func MergeTranslatedConfigs(parent interface{}, parentTranslations translate.TranslationSet, child interface{}, childTranslations translate.TranslationSet) (interface{}, translate.TranslationSet) {
+	// mappings:
+	//   left:  parent or child translate.TranslationSet
+	//   right: merge.Transcript
+
+	// merge configs
+	result, right := merge.MergeStructTranscribe(parent, child)
+
+	// merge left and right mappings into new TranslationSet
+	if parentTranslations.FromTag != childTranslations.FromTag || parentTranslations.ToTag != childTranslations.ToTag {
+		panic(fmt.Sprintf("mismatched translation tags, %s != %s || %s != %s", parentTranslations.FromTag, childTranslations.FromTag, parentTranslations.ToTag, childTranslations.ToTag))
+	}
+	ts := translate.NewTranslationSet(parentTranslations.FromTag, parentTranslations.ToTag)
+	for _, rightEntry := range right.Mappings {
+		var left *translate.TranslationSet
+		switch rightEntry.From.Tag {
+		case merge.TAG_PARENT:
+			left = &parentTranslations
+		case merge.TAG_CHILD:
+			left = &childTranslations
+		default:
+			panic("unexpected mapping tag " + rightEntry.From.Tag)
+		}
+		leftEntry, ok := left.Set[rightEntry.From.String()]
+		if !ok {
+			// the right mapping is more comprehensive than the
+			// left mapping
+			continue
+		}
+		rightEntry.To.Tag = leftEntry.To.Tag
+		ts.AddTranslation(leftEntry.From, rightEntry.To)
+	}
+	return result, ts
+}
