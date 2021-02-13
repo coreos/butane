dc35a4295e320c5bdf2684905feb1c70271a8795
Benjamin Gilbert <bgilbert@redhat.com>
2021-02-12 19:16:21 -0500

    openshift/v4_8_exp: add report correlation test from fcos
    We'll need to generalize it.

---
+++ config/openshift/v4_8_exp/validate_test.go
@@ -0,0 +1,120 @@
+// Copyright 2021 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.)
+
+package v4_8_exp
+
+import (
+	"testing"
+
+	"github.com/coreos/fcct/config/common"
+
+	"github.com/coreos/ignition/v2/config/shared/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestReportCorrelation tests that errors are correctly correlated to their source lines
+func TestReportCorrelation(t *testing.T) {
+	tests := []struct {
+		in      string
+		message string
+		line    int64
+	}{
+		// FCCT unused key check
+		{
+			`storage:
+                           files:
+                           - path: /z
+                             q: z`,
+			"Unused key q",
+			4,
+		},
+		// FCCT YAML validation error
+		{
+			`storage:
+                           files:
+                           - path: /z
+                             contents:
+                               source: https://example.com
+                               inline: z`,
+			common.ErrTooManyResourceSources.Error(),
+			5,
+		},
+		// FCCT YAML validation warning
+		{
+			`storage:
+                           files:
+                           - path: /z
+                             mode: 644`,
+			common.ErrDecimalMode.Error(),
+			4,
+		},
+		// FCCT translation error
+		{
+			`storage:
+                           files:
+                           - path: /z
+                             contents:
+                               local: z`,
+			common.ErrNoFilesDir.Error(),
+			5,
+		},
+		// Ignition validation error, leaf node
+		{
+			`storage:
+                           files:
+                           - path: z`,
+			errors.ErrPathRelative.Error(),
+			3,
+		},
+		// Ignition validation error, partition
+		{
+			`storage:
+                           disks:
+                           - device: /dev/z
+                             partitions:
+                               - start_mib: 5`,
+			errors.ErrNeedLabelOrNumber.Error(),
+			5,
+		},
+		// Ignition validation error, partition list
+		{
+			`storage:
+                           disks:
+                           - device: /dev/z
+                             partitions:
+                               - number: 1
+                                 should_exist: false
+                               - label: z`,
+			errors.ErrZeroesWithShouldNotExist.Error(),
+			5,
+		},
+		// Ignition duplicate key check, paths
+		{
+			`storage:
+                           files:
+                           - path: /z
+                           - path: /z`,
+			errors.ErrDuplicate.Error(),
+			4,
+		},
+	}
+
+	for i, test := range tests {
+		_, r, _ := ToIgn3_2Bytes([]byte(test.in), common.TranslateBytesOptions{})
+		assert.Len(t, r.Entries, 1, "#%d: unexpected report length", i)
+		assert.Equal(t, test.message, r.Entries[0].Message, "#%d: bad error", i)
+		assert.NotNil(t, r.Entries[0].Marker.StartP, "#%d: marker start is nil", i)
+		assert.Equal(t, test.line, r.Entries[0].Marker.StartP.Line, "#%d: incorrect error line", i)
+	}
+}
