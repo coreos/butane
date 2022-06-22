coreos/butane

commit 72825ae54b15c33a231f23417a375999c31c5839

translate/set.go: Add AddFromCommonObject helper
This is a useful function for copying all the fields of one object to the
identically named fields of another object. We add translations prefixed by
fromPrefix and toPrefix, retrieving the subpaths from to.

translate/set.go
@@ -105,6 +105,19 @@ func (ts TranslationSet) AddFromCommonSource(common path.ContextPath, toPrefix p
 	ts.AddTranslation(common, toPrefix)
 }
 
+// AddFromCommonObject adds translations for all of the paths in to. The paths being translated
+// are prefixed by fromPrefix and the translated paths are prefixed by toPrefix.
+// This is useful when we want to copy all the fields of an object to another with the same field names.
+func (ts TranslationSet) AddFromCommonObject(fromPrefix path.ContextPath, toPrefix path.ContextPath, to interface{}) {
+	vTo := reflect.ValueOf(to)
+	vPaths := getAllPaths(vTo, ts.ToTag, true)
+
+	for _, path := range vPaths {
+		ts.AddTranslation(prefixPath(path, fromPrefix.Path...), prefixPath(path, toPrefix.Path...))
+	}
+	ts.AddTranslation(fromPrefix, toPrefix)
+}
+
 // Merge adds all the entries to the set. It mutates the Set in place.
 func (ts TranslationSet) Merge(from TranslationSet) {
 	for _, t := range from.Set {

commit 16ebbb0b38180fe9fbeb265d32c4cd47cba08e34

translate: use FromTag in From paths added by AddFromCommonSource
AddFromCommonSource was using the ToTag in both paths.  Use the FromTag
in the From path, and add a test.

Fixes: 72825ae54b ("translate/set.go: Add AddFromCommonObject helper")

translate/set.go
@@ -113,7 +113,7 @@ func (ts TranslationSet) AddFromCommonObject(fromPrefix path.ContextPath, toPref
 	vPaths := getAllPaths(vTo, ts.ToTag, true)
 
 	for _, path := range vPaths {
-		ts.AddTranslation(prefixPath(path, fromPrefix.Path...), prefixPath(path, toPrefix.Path...))
+		ts.AddTranslation(fromPrefix.Append(path.Path...), toPrefix.Append(path.Path...))
 	}
 	ts.AddTranslation(fromPrefix, toPrefix)
 }

translate/set_test.go
@@ -78,3 +78,23 @@ func TestTranslationSetMap(t *testing.T) {
 		fp("a", 0, "b", "i"), fp("A", 0, "B", 1, "F"),
 	), result, "bad mapping")
 }
+
+func TestTranslationSetAddFromCommonSource(t *testing.T) {
+	type Sub struct {
+		C int `json:"c"`
+	}
+	type Main struct {
+		A string `json:"a"`
+		B Sub    `json:"b"`
+	}
+
+	expected := NewTranslationSet("yaml", "json")
+	expected.AddTranslation(path.New("yaml", "y"), path.New("json", "z", 0))
+	expected.AddTranslation(path.New("yaml", "y", "a"), path.New("json", "z", 0, "a"))
+	expected.AddTranslation(path.New("yaml", "y", "b"), path.New("json", "z", 0, "b"))
+	expected.AddTranslation(path.New("yaml", "y", "b", "c"), path.New("json", "z", 0, "b", "c"))
+
+	actual := NewTranslationSet("yaml", "json")
+	actual.AddFromCommonObject(path.New("yaml", "y"), path.New("json", "z", 0), &Main{})
+	assert.Equal(t, expected, actual)
+}